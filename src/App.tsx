32c3eda743de34ffe6ad19eaf6d66a2cac726bd4
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-09-03 08:25:59 +0900

    :tada: Initializes react application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h2>Kanji Printer</h2>
+        </div>
+    );
+}
+
+export default App;
