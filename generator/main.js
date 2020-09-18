e62af8b092b6ccd48114bc009c4b2ac0a1d923a7
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-09-19 02:44:38 +0900

    :tada: Updates generator to generate smaller PDFs
    - Adds CSS to remove stroke numbers from writing box
    - Updates template page size
    - Increase generation speed with Promise.all

--- generator/main.js
+++ generator/main.js
@@ -1,9 +1,12 @@
+const os = require('os');
 const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
 const puppeteer = require('puppeteer');
+const {default: PQueue} = require('p-queue');
 
 const kanjiVgPrefix = "https://raw.githubusercontent.com/KanjiVG/kanjivg/master/kanji/";
+const cssForSvg = '<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/css" href="../../css/svg-writing.css" ?>'
 
 const outDir = "build"
 const svgOutputDir = `${outDir}/svg`
@@ -22,20 +25,21 @@ const ensureDirectories = (...dirNames) => {
 }
 
 async function processSources(...sourceDirs) {
-    for (const sourceDir of sourceDirs) {
-        await processSourceDir(sourceDir)
-    }
+    await Promise.all(sourceDirs.map(sourceDir => processSourceDir(sourceDir)))
 }
 
 async function generatePDFs(...sourceDirs) {
     for (const sourceDir of sourceDirs) {
         try {
+            let destinationDir = path.join(pdfOutputDir, sourceDir)
+            ensureDirectories(destinationDir)
+
             let filenames = fs.readdirSync(sourceDir);
             console.log(`Filenames in source Dir ${sourceDir} are ${filenames}`)
             for (let sourceFile of filenames) {
                 const inputFilePath = path.join(sourceDir, sourceFile)
-                const outputFilePath = path.join(pdfOutputDir, `${sourceDir}_${sourceFile}.pdf`)
-                await generatePDF(inputFilePath, outputFilePath)
+                const outputDirectoryPath = path.join(pdfOutputDir, sourceDir)
+                await generatePDF(inputFilePath, outputDirectoryPath, sourceFile.split('.')[0])
             }
         } catch (error) {
             logError(`Error while reading directory ${sourceDir}: ${error}`);
@@ -43,14 +47,16 @@ async function generatePDFs(...sourceDirs) {
     }
 }
 
-async function generatePDF(inputFilePath, outputFilePath) {
-    logStart(`Reading file ${inputFilePath}...`);
-    try {
-        const content = fs.readFileSync(inputFilePath, {encoding: 'utf-8', flag: 'r'})
-        const data = content.split('\n').filter(Boolean).join("")
-        console.log(`Data for file ${inputFilePath} is ${data}`)
+async function generatePDF(inputFilePath, outputDirectoryPath, sourceGroup) {
+    const tempDirPath = path.join(outputDirectoryPath, sourceGroup);
+    ensureDirectories(outputDirectoryPath, tempDirPath);
 
-        const timeout = 10 * 60 * 60 * 1000
+    try {
+        logStart(`Reading file ${inputFilePath}...`);
+        const content = fs.readFileSync(inputFilePath, {encoding: 'utf-8', flag: 'r'})
+        const data = content.split('\n').filter(Boolean)
+
+        const timeout = 60 * 1000
         const browser = await puppeteer.launch({
             waitNetworkIdle: true,
             timeout: timeout,
@@ -58,22 +64,39 @@ async function generatePDF(inputFilePath, outputFilePath) {
                 '--disable-web-security',
             ],
         });
-        const page = await browser.newPage();
-        page.setDefaultTimeout(timeout)
 
-        page.on('console', consoleObj => console.log(consoleObj.text()));
-        await page.goto(`file:///${templateAbsolutePath}?data=${data}`, {
-            waitUntil: ['load', 'networkidle0', 'networkidle2', 'domcontentloaded']
-        });
+        // To avoid timeout, if the number of pages are too much, 5 pages per core
+        const browserPageQueue = new PQueue({concurrency: os.cpus().length * 5, autoStart: true});
 
-        await page.waitFor(5 * 60 * 1000)
+        let times = 0
+        for (let index = 0; index < data.length; index += 5) {
+            const processing = async function (index, kanjiArray) {
+                console.log(`${inputFilePath}: Index ${index} start`)
+                const page = await browser.newPage();
+                page.setDefaultTimeout(timeout)
 
-        await page.pdf({
-            path: outputFilePath,
-            displayHeaderFooter: false,
-            printBackground: true,
-            format: 'A4',
-        });
+                page.on('console', consoleObj => console.log(consoleObj.text()));
+                await page.goto(`file:///${templateAbsolutePath}?data=${kanjiArray.join("")}`, {
+                    waitUntil: ['load', 'networkidle0', 'networkidle2', 'domcontentloaded']
+                });
+
+                await page.pdf({
+                    path: path.join(tempDirPath, `${index}.pdf`),
+                    displayHeaderFooter: false,
+                    printBackground: false,
+                    format: 'A4',
+                });
+
+                await page.close()
+                console.log(`${inputFilePath}: Index ${index} finished ${kanjiArray}`)
+            }
+
+            browserPageQueue.add(async () => processing(times++, data.slice(index, index + 5))).then((any) => {
+                console.log(`${inputFilePath}: Index Added to queue`)
+            })
+        }
+
+        await browserPageQueue.onIdle()
         await browser.close();
     } catch (error) {
         logError(`Error on reading file ${inputFilePath} : ${error}`);
@@ -91,9 +114,10 @@ async function processSourceDir(sourceDir) {
             try {
                 const content = fs.readFileSync(inputFile, {encoding: 'utf-8', flag: 'r'})
                 logDone(`Finished reading ${inputFile}`);
-                for (const kanji of content.split('\n').filter(Boolean)) {
-                    await downloadSvg(kanji)
-                }
+                logStart(`Downloading kanji in file ${inputFile}`);
+                const downloads = content.split('\n').filter(Boolean).map(kanji => downloadSvg(kanji))
+                await Promise.all(downloads)
+                logDone(`Downloading kanji in file ${inputFile}`);
             } catch (error) {
                 logError(`Error on reading file ${inputFile} : ${error}`);
             }
@@ -107,13 +131,21 @@ async function processSourceDir(sourceDir) {
 async function downloadSvg(kanji) {
     const readFileUrl = kanjiVgPrefix + "0" + kanji.charCodeAt(0).toString(16) + ".svg";
     const svgOutFilePath = path.join(svgOutputDir, `${kanji}.svg`);
+    const writingSvgOutFilePath = path.join(svgOutputDir, `${kanji}.w.svg`);
     if (fs.existsSync(svgOutFilePath)) {
         // File already downloaded
         return
     }
     const response = await fetch(readFileUrl)
     const content = await response.buffer();
+
+    // Create SVG with stroke numbers
     await fs.writeFileSync(svgOutFilePath, content)
+
+    //Create SVG without stroke
+    const localContent = content.toString().split("\n")
+    localContent[0] = cssForSvg
+    await fs.writeFileSync(writingSvgOutFilePath, localContent.join('\n'))
 }
 
 async function downloadKanjiData() {
@@ -131,8 +163,10 @@ async function generateData() {
     await generatePDFs("frequency", "jlpt", "grade", "wanikani")
 }
 
+console.time('GenerateData');
 generateData().then(function () {
-    console.log("Processing complete.")
+    console.log(`Data Generation finished`);
+    console.timeEnd('GenerateData')
     process.exit(0)
 }).catch(function () {
     process.exit(1)
