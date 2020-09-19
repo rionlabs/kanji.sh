ef3a3b0aa0278d8a91bf2200026186a4bc87e87c
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-09-19 13:42:53 +0900

    :sparkles: Update footer for smaller screen sizes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import common from "@material-ui/core/colors/common";
 import IconButton from "@material-ui/core/IconButton";
 import {GitHub, PublicRounded, Twitter} from "@material-ui/icons";
+import {Grid} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -25,6 +25,9 @@ const useStyles = makeStyles((theme) => ({
         alignSelf: 'center',
         fontSize: 'larger',
         color: '#FFF'
+    },
+    footerRow: {
+        textAlign: 'center'
     },
     link: {
         textDecoration: 'none',
@@ -49,33 +52,37 @@ export default function Footer() {
         <div className={classes.root}>
             <AppBar className={classes.appBar} position="static" color={"transparent"} elevation={0}>
                 <Container>
-                    <Toolbar className={classes.toolbar}>
-                        <Typography className={classes.copyright} variant="subtitle2" noWrap>
-                            Crafted by <a className={classes.link} href="https://aruke.dev">aruke</a> with lots of
-                            Coffee
-                        </Typography>
-                        <IconButton
-                            className={classes.socialButton}
-                            href={"https://github.com/aruke"}
-                            target={"_blank"}
-                            aria-label="open github">
-                            <GitHub/>
-                        </IconButton>
-                        <IconButton
-                            className={classes.socialButton}
-                            href={"https://twitter.com/aruke_"}
-                            target={"_blank"}
-                            aria-label="follow me on twitter">
-                            <Twitter/>
-                        </IconButton>
-                        <IconButton
-                            className={classes.socialButton}
-                            href={"https://aruke.dev"}
-                            target={"_blank"}
-                            aria-label="open my blog/website">
-                            <PublicRounded/>
-                        </IconButton>
-                    </Toolbar>
+                    <Grid container spacing={2} alignItems={"center"}>
+                        <Grid item sm={12} md={6} className={classes.footerRow}>
+                            <Typography className={classes.copyright} variant="subtitle2" noWrap>
+                                Crafted by <a className={classes.link} href="https://aruke.dev">aruke</a> with lots of
+                                Coffee
+                            </Typography>
+                        </Grid>
+                        <Grid item sm={12} md={6} className={classes.footerRow}>
+                            <IconButton
+                                className={classes.socialButton}
+                                href={"https://github.com/aruke"}
+                                target={"_blank"}
+                                aria-label="open github">
+                                <GitHub/>
+                            </IconButton>
+                            <IconButton
+                                className={classes.socialButton}
+                                href={"https://twitter.com/aruke_"}
+                                target={"_blank"}
+                                aria-label="follow me on twitter">
+                                <Twitter/>
+                            </IconButton>
+                            <IconButton
+                                className={classes.socialButton}
+                                href={"https://aruke.dev"}
+                                target={"_blank"}
+                                aria-label="open my blog/website">
+                                <PublicRounded/>
+                            </IconButton>
+                        </Grid>
+                    </Grid>
                 </Container>
             </AppBar>
         </div>
