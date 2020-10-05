fe76a87059914cb7507a29c742652689f5fff7d1
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-10-05 22:19:50 +0900

    :truck: Move app component to app directory

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
-import Header from "./components/Header";
-import FileGroup from "./components/FileGroup";
-import {data} from "./Metadata";
+import Header from "../components/Header";
+import FileGroup from "../components/FileGroup";
+import {data} from "../Metadata";
 import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
-import Jumbotron from "./components/Jumbotron";
-import Acknowledgement from "./components/Acknowledgement";
+import Jumbotron from "../components/Jumbotron";
+import Acknowledgement from "../components/Acknowledgement";
 
 function App() {
     const appTheme = createMuiTheme({
