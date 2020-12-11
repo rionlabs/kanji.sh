ab366e4600774cb1111cdbbbf1747b69f3f818eb
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-12-11 20:25:42 +0900

    :globe_with_meridians: Fixed relative path with absolute ones
     This affected non index pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,9 +11,9 @@ export default class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta charSet="utf-8"/>
-                    <link rel="icon" href={"favicon.ico"}/>
-                    <link rel="apple-touch-icon" href={"logo192.png"}/>
-                    <link rel="manifest" href={"manifest.json"}/>
+                    <link rel="icon" href={"/favicon.ico"}/>
+                    <link rel="apple-touch-icon" href={"/logo192.png"}/>
+                    <link rel="manifest" href={"/manifest.json"}/>
                     <meta name="theme-color" content={theme.palette.primary.dark}/>
                     <link
                         rel="stylesheet"
