a51811b10ac31c83bbda917fa1f472f0d9276bc9
Rajanikant Deshmukh <mail@aruke.dev>
2022-02-11 02:59:18 +0900

    :arrow_up: Updates React, Next, Next-Seo and Typescript

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     // Keeping strictMode to true breaks class generation https://github.com/vercel/next.js/issues/7322
-    reactStrictMode: false
+    reactStrictMode: false,
+    swcMinify: true
 };
