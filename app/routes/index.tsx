ab14205626992c56fda7e477503a4945243f92d5
Rajanikant Deshmukh <mail@aruke.dev>
2022-06-04 05:26:41 +0900

    :truck: Moves index page contents

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,51 @@
+import React from 'react';
+import ServiceCard, { Direction } from 'app/components/molecules/ServiceCard';
+import { ReadingBookAnimation } from 'app/components/atoms/AnimatedImage';
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+        <div className="">
+            {/* READ */}
+            <div className="">
+                <div className="flex flex-col gap-4 justify-center content-center items-center">
+                    <div>
+                        <h4>Your one stop tool for practicing kanji.</h4>
+                    </div>
+                    <div>
+                        <p>
+                            It already has every kanji worksheet for writing, and soon it will
+                            have plenty to practice reading.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            {/* WRITE */}
+            <div className="max-w-screen-sm">
+                <ReadingBookAnimation className="p-4" />
+            </div>
+        </div>
+
+        <div className="flex flex-col gap-16 py-8">
+            <div className="elevated rounded-lg sm:rounded-2xl md:rounded-3xl">
+                <ServiceCard
+                    title={'read'}
+                    subtitle="What's the point of remembering kanji if you can't read them in a sentence, eh? Soon you will get practice sessions here for the kanjis you know so that you will be able to read 飛躍的に through anything."
+                    imageUrl={'/assets/svg/undraw_book_lover.svg'}
+                    imageDirection={Direction.end}
+                />
+            </div>
+            <div className="elevated rounded-lg  sm:rounded-2xl md:rounded-3xl">
+                <ServiceCard
+                    title={'write'}
+                    subtitle={
+                        'Although it seems old school, writing is still one of the best ways to improve language skills. Dive in the stroke orders, and make sense of complex kanjis.'
+                    }
+                    imageUrl={'/assets/svg/undraw_studying.svg'}
+                    imageDirection={Direction.start}
+                />
+            </div>
+        </div>
     </div>
   );
 }
