0afe7bb11c7259f62f949a81e0d8599764d3cf3b
Rajanikant Deshmukh <mail@aruke.dev>
2022-06-04 06:44:19 +0900

    :truck: Moves app and document tags to remix app

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import {
     Links,
     LiveReload,
@@ -8,20 +8,35 @@ import {
     ScrollRestoration
 } from '@remix-run/react';
 import PageLayout from 'app/components/layout/PageLayout';
+import React from 'react';
+import { pdfjs } from 'react-pdf';
 
 import styles from './styles/app.css';
 
-export function links() {
-    return [{ rel: 'stylesheet', href: styles }];
-}
+export const links: LinksFunction = () => ([
+    { rel: 'stylesheet', href: styles },
+    { rel: 'icon', href: '/favicon.ico' },
+    { rel: 'apple-touch-icon', href: '/logo192.png' },
+    { rel: 'manifest', href: '/manifest.json' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+    {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Quicksand:wght@500;700&display=swap'
+    }
+]);
+
 
 export const meta: MetaFunction = () => ({
     charset: 'utf-8',
-    title: 'New Remix App',
-    viewport: 'width=device-width,initial-scale=1'
+    'theme-color': '#000',
+    title: 'Kanji.sh',
+    viewport: 'minimum-scale=1,initial-scale=1,width=device-width'
 });
 
 export default function App() {
+    // Loads PDF.js worker for previews
+    pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
     return (
         <html lang='en'>
         <head>
@@ -29,12 +44,12 @@ export default function App() {
             <Links />
         </head>
         <body>
-            <PageLayout>
-                <Outlet />
-            </PageLayout>
-            <ScrollRestoration />
-            <Scripts />
-            <LiveReload />
+        <PageLayout>
+            <Outlet />
+        </PageLayout>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
         </body>
         </html>
     );
