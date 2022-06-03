3dc6d4abaaefbd922483b89a12844d57cd0c2266
Rajanikant Deshmukh <mail@aruke.dev>
2022-06-04 03:16:43 +0900

    :boom: Migrate to remix application
    Reasons to move away from NextJS (and NX, not present in the branch)
    - Next didn't allow to integrate assets from generator lib to application bundle
    - NX has way may package.json changes to match the build toolkit
    - Remix seems to have better bundling for server code and SSR

---
+++ remix.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  appDirectory: "rapp",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+};
