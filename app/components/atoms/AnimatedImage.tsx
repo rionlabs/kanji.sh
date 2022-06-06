a3441f25d1cfb77cf411fe7b188c24c418f2b553
Rajanikant Deshmukh <mail@aruke.dev>
2022-06-07 00:14:07 +0900

    :art: Changes WIP image

--- app/components/atoms/AnimatedImage.tsx
+++ app/components/atoms/AnimatedImage.tsx
@@ -4,23 +4,25 @@ import lottie from 'lottie-web';
 import construction from '../../assets/animations/construction_in_process.json';
 import reading from '../../assets/animations/reading_book.json';
 import writing from '../../assets/animations/writing.json';
+import printing from '../../assets/animations/printing.json';
 
 type AnimationProps = {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     animationData?: any;
     className: string;
+    loop?: boolean | number;
 };
 
 const AnimatedImage: (props: AnimationProps) => JSX.Element = (props: AnimationProps) => {
     const element = useRef<HTMLDivElement>(null);
     const lottieInstance = useRef<any>();
 
-    const { animationData, className } = props;
+    const { animationData, className, loop } = props;
 
     useEffect(() => {
         if (element.current) {
             lottieInstance.current = lottie.loadAnimation({
-                animationData,
+                animationData, loop,
                 container: element.current
             });
         }
@@ -44,3 +46,7 @@ export const ConstructionAnimation: (props: { className: string }) => JSX.Elemen
 export const WritingAnimation: (props: { className: string }) => JSX.Element = (props: {
     className: string;
 }) => <AnimatedImage className={props.className} animationData={writing} />;
+
+export const PrintingAnimation: (props: { className: string }) => JSX.Element = (props: {
+    className: string;
+}) => <AnimatedImage className={props.className} loop={false} animationData={printing} />;
--- app/routes/read/index.tsx
+++ app/routes/read/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { ConstructionAnimation } from 'app/components/atoms/AnimatedImage';
+import { PrintingAnimation } from 'app/components/atoms/AnimatedImage';
 import SubscriptionForm from 'app/components/molecules/SubscriptionForm';
 
 const ReadPage: React.FC = () => {
     return (
         <div className='container flex flex-col items-stretch gap-4 justify-center'>
             <div className='text-center'>
-                <h4>Ooops!</h4>
+                <h4>Work In Progress!</h4>
             </div>
-            <div className=''>
-                <ConstructionAnimation className='' />
+            <div className='max-w-[320px] mx-auto'>
+                <PrintingAnimation className="opacity-95"/>
             </div>
             <div className='text-center'>
-                <div>I haven&apos;t started adding reading sessions yet.</div>
+                <div>We haven&apos;t started adding reading worksheets yet.</div>
                 <div>If you would like it, signup for the updates. No spams, I promise!</div>
             </div>
             <div>
